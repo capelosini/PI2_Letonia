a163c268a42e04c2f4e5429fa4a39d1f134ed164
Enzzx <bellottie815@gmail.com>
2024-10-26 06:19:09 -0300

    code refactor

--- src/main.c
+++ src/main.c
@@ -6,19 +6,19 @@
 
 CAEngine* engine;
 Scene* mainMenu;
-Scene* stageOne;
-Scene* respawn;
+Scene* gameMap;
+Scene* insideBase;
 Scene* sinopse;
 GameObject* player;
 GameObject* ghostPlayerMenu;
-GameObject* constructionQG;
+GameObject* baseObj;
 GameObject* letterObj;
 Font* lettersFont;
-Text* letterTest;
+Text* letterContent;
 Text* pressEMessage;
 Text* sinopseTchau;
 bool letterPicked = false;
-float leafMatrix[100][3];
+float fallingLeafs[100][3];
 
 int walkIndex = 0;
 
@@ -37,7 +37,7 @@ void onEvent(ALLEGRO_EVENT event, Scene* scene, CAEngine* engine) {
                 pressEMessage->visible = 0;
             }
             else if (letterPicked) {
-                letterTest->visible = !letterTest->visible;
+                letterContent->visible = !letterContent->visible;
             }
         }
     }
@@ -45,17 +45,19 @@ void onEvent(ALLEGRO_EVENT event, Scene* scene, CAEngine* engine) {
 
 
 //stage change
-void onStartRespawn(Scene* scene) {
-    changeScene(engine, respawn);
+void onOpenBase(Scene* scene) {
+    changeScene(engine, insideBase);
 }
-void onStartStageOne(Scene* scene) {
-    player->position = (Vector2){ stageOne->camera.maxLimit.x / 2 - player->width - 10, stageOne->camera.maxLimit.y / 2 + 10 };
-    stageOne->camera.offset = (Vector2){ stageOne->camera.maxLimit.x - engine->displayWidth, stageOne->camera.maxLimit.y - engine->displayHeight };
-    changeScene(engine, stageOne);
+void onOpenGameMap(Scene* scene) {
+    player->position = (Vector2){ gameMap->camera.maxLimit.x / 2 - player->width - 10, gameMap->camera.maxLimit.y / 2 + 10 };
+    gameMap->camera.offset = (Vector2){ gameMap->camera.maxLimit.x - engine->displayWidth, gameMap->camera.maxLimit.y - engine->displayHeight };
+    changeScene(engine, gameMap);
 }
-void onStartSinopse(Scene* scene) {
+void onOpenSinopse(Scene* scene) {
     changeScene(engine, sinopse);
 }
+
+
 //character movement and letter handling
 void gameSceneScript(Scene* self) {
     ALLEGRO_KEYBOARD_STATE keyState;
@@ -84,29 +86,30 @@ void gameSceneScript(Scene* self) {
     if (!(mov.x || mov.y))
         player->animation.index.y = walkIndex;
 
-    if (!(letterPicked)) {
+    if (!letterPicked) {
         if (dist(player->position.x, player->position.y, player->width, player->height, letterObj->position.x, letterObj->position.y, letterObj->width, letterObj->height)
             <= 60) {
             pressEMessage->visible = 1;
         }
         else {
             pressEMessage->visible = 0;
-            letterTest->visible = 0;
+            letterContent->visible = 0;
         }
     }
 }
 
+
 //menu animation
 void mainMenuScript(Scene* self) {
     for (int i = 0; i < 100; i++) {
-        al_draw_filled_rectangle(leafMatrix[i][0], leafMatrix[i][1], leafMatrix[i][0] + 7, leafMatrix[i][1] + 5, al_map_rgb(50, 180, 50));
-        leafMatrix[i][1] += leafMatrix[i][2];
-        leafMatrix[i][0] += 0.5;
+        al_draw_filled_rectangle(fallingLeafs[i][0], fallingLeafs[i][1], fallingLeafs[i][0] + 7, fallingLeafs[i][1] + 5, al_map_rgb(50, 180, 50));
+        fallingLeafs[i][1] += fallingLeafs[i][2];
+        fallingLeafs[i][0] += 0.5;
 
-        if (leafMatrix[i][1] > engine->displayHeight) {
-            leafMatrix[i][0] = randInt(-100, engine->displayWidth - 5);
-            leafMatrix[i][1] = randInt(-20, 0);
-            leafMatrix[i][2] = randFloat(1, 2);
+        if (fallingLeafs[i][1] > engine->displayHeight) {
+            fallingLeafs[i][0] = randInt(-100, engine->displayWidth - 5);
+            fallingLeafs[i][1] = randInt(-20, 0);
+            fallingLeafs[i][2] = randFloat(1, 2);
         }
     }
 
@@ -125,6 +128,7 @@ void mainMenuScript(Scene* self) {
 
 }
 
+
 int main() {
     GameConfig engineConfig;
     engineConfig.fps = 60;
@@ -138,13 +142,14 @@ int main() {
     engine = initEngine(engineConfig);
     setEventFunction(engine, onEvent);
 
-    // MENU
+
+    // - - - MENU - - -
     mainMenu = createScene(engine, mainMenuScript);
 
     for (int i = 0; i < 100; i++) {
-        leafMatrix[i][0] = randInt(-100, engine->displayWidth - 5);
-        leafMatrix[i][1] = randInt(-500, 10);
-        leafMatrix[i][2] = randFloat(1, 2);
+        fallingLeafs[i][0] = randInt(-100, engine->displayWidth - 5);
+        fallingLeafs[i][1] = randInt(-500, 10);
+        fallingLeafs[i][2] = randFloat(1, 2);
     }
 
     Font* titleFont = loadTTF(engine, "./assets/fonts/kalam-bold.ttf", 80);
@@ -153,7 +158,7 @@ int main() {
     char* titleText = "Revolução Em Cartas";
     addText(titleText, engine->displayWidth / 2 - al_get_text_width(titleFont->font, titleText) / 2, 50, 0, al_map_rgb(255, 255, 255), al_map_rgba(0, 0, 0, 0), NULL, titleFont, 0, 0, mainMenu);
 
-    addButtonToScene(mainMenu, createButton(engine, engine->displayWidth / 2 - 75, engine->displayHeight / 2 - 25, 150, 50, al_map_rgb(217, 95, 54), al_map_rgb(255, 255, 255), "Jogar", "./assets/fonts/roboto.ttf", NULL, onStartRespawn));
+    addButtonToScene(mainMenu, createButton(engine, engine->displayWidth / 2 - 75, engine->displayHeight / 2 - 25, 150, 50, al_map_rgb(217, 95, 54), al_map_rgb(255, 255, 255), "Jogar", "./assets/fonts/roboto.ttf", NULL, onOpenBase));
 
     mainMenu->backgroundColor = al_map_rgb(0, 0, 20);
 
@@ -168,24 +173,26 @@ int main() {
 
     changeScene(engine, mainMenu);
 
-    //BASE
-    respawn = createScene(engine, gameSceneScript);
-    respawn->camera.zoom = 2;
-    respawn->camera.minLimit.x = 50;
-    respawn->camera.minLimit.y = 50;
-    respawn->camera.maxLimit.x = 1400;
-    respawn->camera.maxLimit.y = 1400;
-    setupSceneWorld(respawn, loadBitmap(engine, "./assets/images/map.png"), 1400, 1400);
-    addWorldTile(respawn, 0, 0, 0, 0);
-    GameObject* respawnMap = createGameObject(SOLID, 50, 50, 1350, 1350, respawn);
-    respawnMap->color = al_map_rgba(0, 0, 0, 0);
-    respawnMap->collisionEnabled = 1;
-    respawnMap->invertedCollision = 1;
-    addButtonToScene(respawn, createButton(engine, 0, 0, 100, 50, al_map_rgb(150, 120, 70), al_map_rgb(20, 20, 20), "Mundo Aberto", "./assets/fonts/roboto.ttf", NULL, onStartStageOne));
+    // - - - BASE - - -
+    insideBase = createScene(engine, gameSceneScript);
+    insideBase->camera.minLimit.x = 0;
+    insideBase->camera.minLimit.y = 0;
+    insideBase->camera.maxLimit.x = 1000;
+    insideBase->camera.maxLimit.y = 1000;
+    insideBase->camera.zoom = 1.5;
+    setupSceneWorld(insideBase, loadBitmap(engine, "./assets/images/map.png"), 1000, 1000);
+    addWorldTile(insideBase, 0, 0, 0, 0);
+    GameObject* baseRoom = createGameObject(SOLID, 20, 30, 960, 950, insideBase);
+    baseRoom->color = al_map_rgba(0, 0, 0, 0);
+    baseRoom->collisionEnabled = 1;
+    baseRoom->invertedCollision = 1;
+    
+    //ao invés do botão, criar um if dentro de um swich case no while e verificar (player.y >= 950 & player.x >= esquerda da escada & <= direita da escada) que ative onOpenGameMap
+    addButtonToScene(insideBase, createButton(engine, 0, 0, 100, 50, al_map_rgb(150, 120, 70), al_map_rgb(20, 20, 20), "Mundo Aberto", "./assets/fonts/roboto.ttf", NULL, onOpenGameMap));
 
-    player = createGameObject(ANIMATED_SPRITE, 700, 50, 44, 50, respawn);
-    player->position.x = 600;
-    player->position.y = 400;
+    player = createGameObject(ANIMATED_SPRITE, 700, 50, 44, 50, insideBase);
+    player->position.x = 100;
+    player->position.y = 50;
     player->physics.enabled = 1;
     player->physics.friction = 0.4;
     player->physics.maxSpeed = 4;
@@ -193,54 +200,54 @@ int main() {
     player->collisionType = COLLISION_RECT;
     player->startCollisionOffset.y = 15;
     setGameObjectAnimation(player, loadBitmap(engine, "./assets/images/player-sprite-sheet.png"), 17, 20, 8, 15);
-    respawn->camera.followTarget = player;
+    insideBase->camera.followTarget = player;
 
-
-    // MAPA DO JOGO
-    stageOne = createScene(engine, gameSceneScript);
-    stageOne->camera.maxLimit.x = 200 * 16;
-    stageOne->camera.maxLimit.y = 100 * 16;
-    stageOne->camera.minLimit.x = 0;
-    stageOne->camera.minLimit.y = 0;
-    stageOne->camera.zoom = 2;
-    stageOne->camera.followTarget = player;
-
-    constructionQG = createGameObject(SPRITE, stageOne->camera.maxLimit.x / 2, stageOne->camera.maxLimit.y / 2, 160, 160, stageOne);
-    setGameObjectBitmap(constructionQG, loadBitmap(engine, "./assets/images/QG.png"));
-
-    constructionQG->collisionEnabled = 1;
-    constructionQG->collisionType = COLLISION_RECT;
-    constructionQG->startCollisionOffset.x = 20;
-    constructionQG->startCollisionOffset.y = 30;
-    constructionQG->endCollisionOffset.x = -20;
-    constructionQG->endCollisionOffset.y = -15;
-
-    letterObj = createGameObject(ANIMATED_SPRITE, 200, 200, 12, 12, stageOne);
+    letterObj = createGameObject(ANIMATED_SPRITE, 500, 280, 12, 12, insideBase);
     setGameObjectAnimation(letterObj, loadBitmap(engine, "./assets/images/letter-sheet.png"), 12, 12, 5, 16);
-    //letterObj->collisionEnabled = 1;
+    //como o letterContent tá sendo passado para >base, quando tenta abrir a carta em >gameMap ele não abre já que não está nessa cena e não dá pra incluir letterContent em >gameMap pq o addGameObjectToScene() não aceita parâmetro do tipo Text*
+    //dependendo da resolução vai precisar mudar dentro de gameSceneScript que é onde gerencia a visibilidade da carta 
+    letterContent = addText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut tincidunt elit. Nunc a magna at nulla tempor iaculis. Curabitur at enim sollicitudin, varius nisi vel, viverra odio. Ut porta metus sed metus gravida elementum. Pellentesque ut mi id quam euismod convallis. Duis vulputate tempus sagittis. Quisque aliquam justo justo, eget lobortis neque tempor non.Integer porta volutpat turpis, nec venenatis ante volutpat sit amet. Proin condimentum vitae augue id tincidunt. Donec tristique lectus non dui pellentesque tincidunt. In sit amet leo suscipit, feugiat leo id, condimentum tellus. Proin vel tempor metus. Mauris in auctor velit. Donec justo justo, iaculis eget pellentesque eget, interdum a nibh. Aenean tincidunt tempor sem. Integer eget elementum metus. Suspendisse non fringilla nunc, sit amet suscipit diam.Suspendisse a justo lorem. Phasellus ac nulla sed arcu fermentum sollicitudin.",
+        300, 200, 250, al_map_rgb(0, 0, 0), al_map_rgba(155, 122, 73, 200), NULL, lettersFont, 40, 20, insideBase);
+    letterContent->visible = 0;
 
-    addGameObjectToScene(stageOne, player);
-
-    letterTest = addText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut tincidunt elit. Nunc a magna at nulla tempor iaculis. Curabitur at enim sollicitudin, varius nisi vel, viverra odio. Ut porta metus sed metus gravida elementum. Pellentesque ut mi id quam euismod convallis. Duis vulputate tempus sagittis. Quisque aliquam justo justo, eget lobortis neque tempor non.Integer porta volutpat turpis, nec venenatis ante volutpat sit amet. Proin condimentum vitae augue id tincidunt. Donec tristique lectus non dui pellentesque tincidunt. In sit amet leo suscipit, feugiat leo id, condimentum tellus. Proin vel tempor metus. Mauris in auctor velit. Donec justo justo, iaculis eget pellentesque eget, interdum a nibh. Aenean tincidunt tempor sem. Integer eget elementum metus. Suspendisse non fringilla nunc, sit amet suscipit diam.Suspendisse a justo lorem. Phasellus ac nulla sed arcu fermentum sollicitudin.",
-        300, 200, 250, al_map_rgb(0, 0, 0), al_map_rgba(155, 122, 73, 100), NULL, lettersFont, 40, 20, stageOne);
-    letterTest->visible = 0;
-
-    pressEMessage = addText("Pressione E para pegar a carta", engine->displayWidth / 2, 200, 0, al_map_rgb(255, 255, 255), al_map_rgba(0, 0, 0, 100), NULL, stdMessageFont, 40, 20, stageOne);
+    pressEMessage = addText("Pressione E para pegar a carta", engine->displayWidth / 2, 200, 0, al_map_rgb(255, 255, 255), al_map_rgba(0, 0, 0, 100), NULL, stdMessageFont, 40, 20, insideBase);
     pressEMessage->position.x -= al_get_text_width(stdMessageFont->font, pressEMessage->text) / 2;
     pressEMessage->visible = 0;
 
-    //setupSceneWorld(stageOne, loadBitmap(engine, "./assets/images/map.png"), 3840, 2560);
-    setupSceneWorld(stageOne, loadBitmap(engine, "./assets/images/map-sheet.png"), 16, 16);
-    loadMap("./map.CAE", stageOne);
-    GameObject* map = createGameObject(SOLID, 0, 0, 200 * 16, 100 * 16, stageOne);
+
+    // - - - MAPA DO JOGO - - -
+    gameMap = createScene(engine, gameSceneScript);
+    gameMap->camera.minLimit.x = 0;
+    gameMap->camera.minLimit.y = 0;
+    gameMap->camera.maxLimit.x = 200 * 16;
+    gameMap->camera.maxLimit.y = 100 * 16;
+    gameMap->camera.zoom = 2;
+    gameMap->camera.followTarget = player;
+
+    //setupSceneWorld(gameMap, loadBitmap(engine, "./assets/images/map.png"), 3840, 2560);
+    setupSceneWorld(gameMap, loadBitmap(engine, "./assets/images/map-sheet.png"), 16, 16);
+    loadMap("./map.CAE", gameMap);
+    GameObject* map = createGameObject(SOLID, 0, 0, 200 * 16, 100 * 16, gameMap);
     map->color = al_map_rgba(0, 0, 0, 0);
     map->collisionEnabled = 1;
     map->collisionType = COLLISION_RECT;
     map->invertedCollision = 1;
 
-    addButtonToScene(stageOne, createButton(engine, 0, 0, 100, 50, al_map_rgb(150, 120, 70), al_map_rgb(20, 20, 20), "sinopse", "./assets/fonts/roboto.ttf", NULL, onStartSinopse));
+    addGameObjectToScene(gameMap, player);
+    //addGameObjectToScene(gameMap, letterContent);
 
-    //SINOPSE
+    baseObj = createGameObject(SPRITE, gameMap->camera.maxLimit.x / 2 - 335, gameMap->camera.maxLimit.y / 2 - 330, 335, 330, gameMap);
+    setGameObjectBitmap(baseObj, loadBitmap(engine, "./assets/images/base.png"));
+    baseObj->collisionEnabled = 1;
+    baseObj->collisionType = COLLISION_RECT;
+    baseObj->startCollisionOffset.x = 20;
+    baseObj->startCollisionOffset.y = 30;
+    baseObj->endCollisionOffset.x = -20;
+    baseObj->endCollisionOffset.y = -15;
+
+    //addButtonToScene(gameMap, createButton(engine, 0, 0, 100, 50, al_map_rgb(150, 120, 70), al_map_rgb(20, 20, 20), "sinopse", "./assets/fonts/roboto.ttf", NULL, onOpenSinopse));
+
+    // - - - SINOPSE - - -
     sinopse = createScene(engine, NULL);
     sinopse->backgroundColor = al_map_rgb(20, 20, 20);
     lettersFont->size = 200;
@@ -249,12 +256,10 @@ int main() {
 
 
     while (engine->isAlive) {
-        printf("x = %f  e y = %f \n", player->position.x, player->position.y);
         render(engine);
     }
 
     freeEngine(engine);
-
     printf("\nSuccessfully exited!\n");
 
     return 0;
