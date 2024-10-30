e560e433f6fd4c3f6c605c0632d06fa40fca6f00
Matheus Henrique <104656226+mathh412@users.noreply.github.com>
2024-10-30 16:56:23 -0300

    Quartel Collisions finished and var names modified

--- src/main.c
+++ src/main.c
@@ -6,15 +6,17 @@
 
 CAEngine* engine;
 Scene* mainMenu;
-Scene* gameMap;
 Scene* insideBase;
+Scene* gameMap;
+Scene* quartel;
 Scene* sinopse;
 GameObject* player;
 GameObject* ghostPlayerMenu;
 GameObject* baseObj;
 GameObject* letterObj;
-GameObject* obj8;
+GameObject* exitBase;
 GameObject* returnBase;
+GameObject* quartelobj;
 Font* lettersFont;
 Text* letterContent;
 Text* pressEMessage;
@@ -60,13 +62,20 @@ void onOpenGameMap(Scene* scene) {
     gameMap->camera.offset = (Vector2){ player->position.x, player->position.y };
     changeScene(engine, gameMap);
 }
+void onOpenQuartel(Scene* scene)
+{
+    player->position = (Vector2){ quartelobj->position.x + quartelobj->width / 2 - 100 ,  quartelobj->height - 60 };
+    quartel->camera.offset = (Vector2){ player->position.x, player->position.y };
+    changeScene(engine, quartel);
+
+}
 void onOpenSinopse(Scene* scene) {
     changeScene(engine, sinopse);
 }
 
 void onPlayerCollision(GameObject* self, GameObject* obj)
 {
-    if(obj == obj8)
+    if(obj == exitBase)
     onOpenGameMap(NULL);
     if (obj == returnBase)
     onOpenBase(NULL);
@@ -173,9 +182,9 @@ int main() {
     Font* stdMessageFont = loadTTF(engine, "./assets/fonts/kalam.ttf", 17);
     lettersFont = loadTTF(engine, "./assets/fonts/kalam.ttf", 10);
     char* titleText = "Revolução Em Cartas";
-    createText(titleText, engine->displayWidth / 2 - al_get_text_width(titleFont->font, titleText) / 2, 50, 0, al_map_rgb(255, 255, 255), al_map_rgba(0, 0, 0, 0), NULL, titleFont, 0, 0, mainMenu);
+    addText(titleText, engine->displayWidth / 2 - al_get_text_width(titleFont->font, titleText) / 2, 50, 0, al_map_rgb(255, 255, 255), al_map_rgba(0, 0, 0, 0), NULL, titleFont, 0, 0, mainMenu);
 
-    addButtonToScene(mainMenu, createButton(engine, engine->displayWidth / 2 - 75, engine->displayHeight / 2 - 25, 150, 50, al_map_rgb(217, 95, 54), al_map_rgb(255, 255, 255), "Jogar", "./assets/fonts/roboto.ttf", NULL, onOpenBase));
+    addButtonToScene(mainMenu, createButton(engine, engine->displayWidth / 2 - 75, engine->displayHeight / 2 - 25, 150, 50, al_map_rgb(217, 95, 54), al_map_rgb(255, 255, 255), "Jogar", "./assets/fonts/roboto.ttf", NULL, onOpenQuartel));
 
     mainMenu->backgroundColor = al_map_rgb(0, 0, 20);
 
@@ -204,18 +213,16 @@ int main() {
     baseRoom->collisionEnabled = 1;
     baseRoom->invertedCollision = 1;
     
-    //ao invés do botão, criar um if dentro de um swich case no while e verificar (player.y >= 950 & player.x >= esquerda da escada & <= direita da escada) que ative onOpenGameMap
-    addButtonToScene(insideBase, createButton(engine, 0, 0, 100, 50, al_map_rgb(150, 120, 70), al_map_rgb(20, 20, 20), "Mundo Aberto", "./assets/fonts/roboto.ttf", NULL, onOpenGameMap));
 
     letterObj = createGameObject(ANIMATED_SPRITE, 467, 280, 12, 12, insideBase);
     setGameObjectAnimation(letterObj, loadBitmap(engine, "./assets/images/letter-sheet.png"), 12, 12, 5, 16);
     //como o letterContent tá sendo passado para >base, quando tenta abrir a carta em >gameMap ele não abre já que não está nessa cena e não dá pra incluir letterContent em >gameMap pq o addGameObjectToScene() não aceita parâmetro do tipo Text*
     //dependendo da resolução vai precisar mudar dentro de gameSceneScript que é onde gerencia a visibilidade da carta 
-    letterContent = createText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut tincidunt elit. Nunc a magna at nulla tempor iaculis. Curabitur at enim sollicitudin, varius nisi vel, viverra odio. Ut porta metus sed metus gravida elementum. Pellentesque ut mi id quam euismod convallis. Duis vulputate tempus sagittis. Quisque aliquam justo justo, eget lobortis neque tempor non.Integer porta volutpat turpis, nec venenatis ante volutpat sit amet. Proin condimentum vitae augue id tincidunt. Donec tristique lectus non dui pellentesque tincidunt. In sit amet leo suscipit, feugiat leo id, condimentum tellus. Proin vel tempor metus. Mauris in auctor velit. Donec justo justo, iaculis eget pellentesque eget, interdum a nibh. Aenean tincidunt tempor sem. Integer eget elementum metus. Suspendisse non fringilla nunc, sit amet suscipit diam.Suspendisse a justo lorem. Phasellus ac nulla sed arcu fermentum sollicitudin.",
+    letterContent = addText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut tincidunt elit. Nunc a magna at nulla tempor iaculis. Curabitur at enim sollicitudin, varius nisi vel, viverra odio. Ut porta metus sed metus gravida elementum. Pellentesque ut mi id quam euismod convallis. Duis vulputate tempus sagittis. Quisque aliquam justo justo, eget lobortis neque tempor non.Integer porta volutpat turpis, nec venenatis ante volutpat sit amet. Proin condimentum vitae augue id tincidunt. Donec tristique lectus non dui pellentesque tincidunt. In sit amet leo suscipit, feugiat leo id, condimentum tellus. Proin vel tempor metus. Mauris in auctor velit. Donec justo justo, iaculis eget pellentesque eget, interdum a nibh. Aenean tincidunt tempor sem. Integer eget elementum metus. Suspendisse non fringilla nunc, sit amet suscipit diam.Suspendisse a justo lorem. Phasellus ac nulla sed arcu fermentum sollicitudin.",
         300, 200, 250, al_map_rgb(0, 0, 0), al_map_rgba(155, 122, 73, 200), NULL, lettersFont, 40, 20, insideBase);
     letterContent->visible = 0;
 
-    pressEMessage = createText("Pressione E para pegar a carta", engine->displayWidth / 2, 200, 0, al_map_rgb(255, 255, 255), al_map_rgba(0, 0, 0, 100), NULL, stdMessageFont, 40, 20, insideBase);
+    pressEMessage = addText("Pressione E para pegar a carta", engine->displayWidth / 2, 200, 0, al_map_rgb(255, 255, 255), al_map_rgba(0, 0, 0, 100), NULL, stdMessageFont, 40, 20, insideBase);
     pressEMessage->position.x -= al_get_text_width(stdMessageFont->font, pressEMessage->text) / 2;
     pressEMessage->visible = 0;
 
@@ -227,34 +234,34 @@ int main() {
     player->physics.maxSpeed = 4;
     player->collisionEnabled = 1;
     player->collisionType = COLLISION_RECT;
-    player->startCollisionOffset.y = 15;
+    player->startCollisionOffset.y = 35;
     player->onCollision = onPlayerCollision;
     setGameObjectAnimation(player, loadBitmap(engine, "./assets/images/player-sprite-sheet.png"), 17, 20, 8, 15);
     insideBase->camera.followTarget = player;
-    GameObject*  obj1 = createGameObject(SOLID, 10, 10, 78, 38, insideBase);
-    obj1 ->color = al_map_rgba(0, 0, 0, 0);
-    obj1->collisionEnabled = 1 ;
-    GameObject* obj2 = createGameObject(SOLID, 10, 150, 17, 70, insideBase);
-    obj2->color = al_map_rgba(0, 0, 0, 0);
-    obj2->collisionEnabled = 1;
-    GameObject* obj3 = createGameObject(SOLID, 10, 275, 17, 45, insideBase);
-    obj3->color = al_map_rgba(0, 0, 0, 0);
-    obj3->collisionEnabled = 1;
-    GameObject* obj4 = createGameObject(SOLID, 340, 175, 300, 100, insideBase);
-    obj4->color = al_map_rgba(0, 0, 0, 0);
-    obj4->collisionEnabled = 1;
-    GameObject* obj5 = createGameObject(SOLID, 940, 20, 30 , 105, insideBase);
-    obj5->color = al_map_rgba(0, 0, 0, 0);
-    obj5->collisionEnabled = 1;
-    GameObject* obj6 = createGameObject(SOLID, 975, 175, 30, 100, insideBase);
-    obj6->color = al_map_rgba(0, 0, 0, 0);
-    obj6->collisionEnabled = 1;
-    GameObject* obj7 = createGameObject(SOLID, 975, 325, 30, 45, insideBase);
-    obj7->color = al_map_rgba(0, 0, 0, 0);
-    obj7->collisionEnabled = 1;
-    obj8 = createGameObject(SOLID, 350, 985, 275, 10, insideBase); 
-    obj8->color = al_map_rgba(0, 0, 0, 0); 
-    obj8->collisionEnabled = 1; 
+    GameObject*  boxl = createGameObject(SOLID, 20, 20, 78, 50, insideBase);
+    boxl ->color = al_map_rgba(0, 0, 0, 0);
+    boxl->collisionEnabled = 1 ;
+    GameObject* benchl1 = createGameObject(SOLID, 20, 150, 17, 95, insideBase);
+    benchl1->color = al_map_rgba(0, 0, 0, 0);
+    benchl1->collisionEnabled = 1;
+    GameObject* benchl2 = createGameObject(SOLID, 20, 275, 17, 72, insideBase);
+    benchl2->color = al_map_rgba(0, 0, 0, 0);
+    benchl2->collisionEnabled = 1;
+    GameObject* table = createGameObject(SOLID, 340, 175, 300, 115, insideBase);
+    table->color = al_map_rgba(0, 0, 0, 0);
+    table->collisionEnabled = 1;
+    GameObject* boxr = createGameObject(SOLID, 940, 20, 30 , 125, insideBase);
+    boxr->color = al_map_rgba(0, 0, 0, 0);
+    boxr->collisionEnabled = 1;
+    GameObject* benchr1 = createGameObject(SOLID, 960, 175, 30, 115, insideBase);
+    benchr1->color = al_map_rgba(0, 0, 0, 0);
+    benchr1->collisionEnabled = 1;
+    GameObject* benchr2 = createGameObject(SOLID, 960, 325, 30, 63, insideBase);
+    benchr2->color = al_map_rgba(0, 0, 0, 0);
+    benchr2->collisionEnabled = 1;
+    exitBase = createGameObject(SOLID, 350, 985, 275, 10, insideBase); 
+    exitBase->color = al_map_rgba(0, 0, 0, 0); 
+    exitBase->collisionEnabled = 1; 
 
 
 
@@ -297,30 +304,184 @@ int main() {
     returnBase = createGameObject(SOLID, baseObj->position.x+ 170, baseObj->position.y +245, 5, 2, gameMap);
     returnBase->color = al_map_rgba(0, 0, 0, 0);
     returnBase->collisionEnabled = 1;
-    GameObject* left = createGameObject(SOLID, baseObj->position.x + 79 , baseObj->position.y + 293, 55, 1, gameMap);
-    left->color = al_map_rgba(0, 0, 0, 0);
-    left->collisionEnabled = 1;
-    GameObject* left2 = createGameObject(SOLID, baseObj->position.x + 79, baseObj->position.y + 280, 1, 10, gameMap);
-    left2->color = al_map_rgba(0, 0, 0, 0);
-    left2->collisionEnabled = 1;
-    GameObject* right = createGameObject(SOLID, baseObj->position.x + 213, baseObj->position.y + 293, 59, 1, gameMap);
-    right->color = al_map_rgba(0, 0, 0, 0);
-    right->collisionEnabled = 1;
-    GameObject* right2 = createGameObject(SOLID, baseObj->position.x + 273, baseObj->position.y + 278, 1, 13, gameMap);
-    right2->color = al_map_rgba(0, 0, 0, 0);
-    right2->collisionEnabled = 1;
-
-  
-    //addGameObjectToScene(gameMap, letterContent);
+    GameObject* gridl1 = createGameObject(SOLID, baseObj->position.x + 79 , baseObj->position.y + 293, 55, 1, gameMap);
+    gridl1->color = al_map_rgba(0, 0, 0, 0);
+    gridl1->collisionEnabled = 1;
+    GameObject* gridl2 = createGameObject(SOLID, baseObj->position.x + 79, baseObj->position.y + 280, 1, 10, gameMap);
+    gridl2->color = al_map_rgba(0, 0, 0, 0);
+    gridl2->collisionEnabled = 1;
+    GameObject* gridr1 = createGameObject(SOLID, baseObj->position.x + 213, baseObj->position.y + 293, 59, 1, gameMap);
+    gridr1->color = al_map_rgba(0, 0, 0, 0);
+    gridr1->collisionEnabled = 1;
+    GameObject* gridr2 = createGameObject(SOLID, baseObj->position.x + 273, baseObj->position.y + 278, 1, 13, gameMap);
+    gridr2->color = al_map_rgba(0, 0, 0, 0);
+    gridr2->collisionEnabled = 1;
 
 
-    //addButtonToScene(gameMap, createButton(engine, 0, 0, 100, 50, al_map_rgb(150, 120, 70), al_map_rgb(20, 20, 20), "sinopse", "./assets/fonts/roboto.ttf", NULL, onOpenSinopse));
+    //- - - QUARTEL - - -
+    quartel = createScene(engine, gameSceneScript);
+    quartel->camera.minLimit.x = 0;
+    quartel->camera.minLimit.y = 0;
+    quartel->camera.maxLimit.x = 2500;
+    quartel->camera.maxLimit.y = 2500;
+    quartel->camera.zoom = 1;
+    quartel->camera.followTarget = player;
+    setupSceneWorld(quartel, loadBitmap(engine, "./assets/images/quartel.png"), 2500, 2500);
+    addWorldTile(quartel, 0, 0, 0, 0);
+    addGameObjectToScene(quartel, player);
+    quartelobj = createGameObject(SOLID, 0, 0, 2500, 2500, quartel);
+    quartelobj->color = al_map_rgba(0, 0, 0, 0);
+    quartelobj->collisionEnabled = 1;
+    quartelobj->collisionType = COLLISION_RECT;
+    quartelobj->invertedCollision = 1;
+
+    //LAGO DA ESQUERDA
+    GameObject* pondl1 = createGameObject(SOLID, 106, 1766, 750, 600, quartel);
+    pondl1->color = al_map_rgba(0, 0, 0, 0);
+    pondl1->collisionEnabled = 1;
+    pondl1->collisionType = COLLISION_RECT;
+    GameObject* pondl2 = createGameObject(SOLID, 125, 1735, 700, 10, quartel);
+    pondl2->color = al_map_rgba(0, 0, 0, 0);
+    pondl2->collisionEnabled = 1;
+    pondl2->collisionType = COLLISION_RECT;
+    GameObject* pondl3 = createGameObject(SOLID, 115, 1745, 725, 25, quartel);
+    pondl3->color = al_map_rgba(0, 0, 0, 0);
+    pondl3->collisionEnabled = 1;
+    pondl3->collisionType = COLLISION_RECT;
+    GameObject* pondl4 = createGameObject(SOLID, 855, 1790, 30, 600, quartel);
+    pondl4->color = al_map_rgba(0, 0, 0, 0);
+    pondl4->collisionEnabled = 1;
+    pondl4->collisionType = COLLISION_RECT;
+    GameObject* pondl5 = createGameObject(SOLID, 885, 1820, 30, 570, quartel);
+    pondl5->color = al_map_rgba(0, 0, 0, 0);
+    pondl5->collisionEnabled = 1;
+    pondl5->collisionType = COLLISION_RECT;
+    GameObject* pondl6 = createGameObject(SOLID, 915, 1850, 30, 540, quartel);
+    pondl6->color = al_map_rgba(0, 0, 0, 0);
+    pondl6->collisionEnabled = 1;
+    pondl6->collisionType = COLLISION_RECT;
+    GameObject* pondl7 = createGameObject(SOLID, 945, 1880, 30, 510, quartel);
+    pondl7->color = al_map_rgba(0, 0, 0, 0);
+    pondl7->collisionEnabled = 1;
+    pondl7->collisionType = COLLISION_RECT;
+    GameObject* pondl8 = createGameObject(SOLID, 975, 1910, 30, 471, quartel);
+    pondl8->color = al_map_rgba(0, 0, 0, 0);
+    pondl8->collisionEnabled = 1;
+    pondl8->collisionType = COLLISION_RECT;
+    GameObject* pondl9 = createGameObject(SOLID, 126, 2350, 730, 40, quartel);
+    pondl9->color = al_map_rgba(0, 0, 0, 0);
+    pondl9->collisionEnabled = 1;
+    pondl9->collisionType = COLLISION_RECT;
+
+    //LAGO DA DIREITA
+    GameObject* pondr1 = createGameObject(SOLID, 1320, 1755, 1070, 603, quartel);
+    pondr1->color = al_map_rgba(0, 0, 0, 00);
+    pondr1->collisionEnabled = 1;
+    pondr1->collisionType = COLLISION_RECT;
+    GameObject* pondr2 = createGameObject(SOLID, 1335, 2366, 1046, 18, quartel);
+    pondr2->color = al_map_rgba(0, 0, 0, 0);
+    pondr2->collisionEnabled = 1;
+    pondr2->collisionType = COLLISION_RECT;
+    GameObject* pondr3 = createGameObject(SOLID, 1260, 1736, 1110, 70, quartel);
+    pondr3->color = al_map_rgba(0, 0, 0, 0);
+    pondr3->collisionEnabled = 1;
+    pondr3->collisionType = COLLISION_RECT;
+    GameObject* pondr4 = createGameObject(SOLID, 1240, 1750, 30, 30, quartel);
+    pondr4->color = al_map_rgba(0, 0, 0, 0);
+    pondr4->collisionEnabled = 1;
+    pondr4->collisionType = COLLISION_RECT;
+    GameObject* pondr5 = createGameObject(SOLID, 1285, 1810, 30, 30, quartel);
+    pondr5->color = al_map_rgba(0, 0, 0, 0);
+    pondr5->collisionEnabled = 1;
+    pondr5->collisionType = COLLISION_RECT;
+
+    //QUARTEL
+
+    GameObject* wallL1 = createGameObject(SOLID, 140, 640, 10, 1000, quartel);
+    wallL1->color = al_map_rgba(0, 0, 0, 0);
+    wallL1->collisionEnabled = 1;
+    wallL1->collisionType = COLLISION_RECT;
+    GameObject*  wallL2 = createGameObject(SOLID, 140, 640, 240, 313, quartel);
+    wallL2->color = al_map_rgba(0, 0, 0, 0);
+    wallL2->collisionEnabled = 1;
+    wallL2->collisionType = COLLISION_RECT;
+    GameObject* wallL3 = createGameObject(SOLID, 140, 1060, 260, 380, quartel);
+    wallL3->color = al_map_rgba(0, 0, 0, 0);
+    wallL3->collisionEnabled = 1;
+    wallL3->collisionType = COLLISION_RECT;
+    GameObject* wallL4 = createGameObject(SOLID, 140, 1440, 240, 250, quartel);
+    wallL4->color = al_map_rgba(0, 0, 0, 0);
+    wallL4->collisionEnabled = 1;
+    wallL4->collisionType = COLLISION_RECT;
+    GameObject* wallL5 = createGameObject(SOLID, 380, 1590, 20, 50, quartel);
+    wallL5->color = al_map_rgba(0, 0, 0, 0);
+    wallL5->collisionEnabled = 1;
+    wallL5->collisionType = COLLISION_RECT;
+
+    GameObject* wallD1= createGameObject(SOLID, 380, 1680, 520, 10, quartel);
+    wallD1->color = al_map_rgba(0, 0, 0, 0);
+    wallD1->collisionEnabled = 1;
+    GameObject* wallD2 = createGameObject(SOLID, 1140, 1680, 1220, 10, quartel);
+    wallD2->color = al_map_rgba(0, 0, 0, 0);
+    wallD1->collisionType = COLLISION_RECT;
+    wallD2->collisionEnabled = 1;
+    wallD2->collisionType = COLLISION_RECT;
+
+    GameObject* wallR1 = createGameObject(SOLID, 2120, 640, 240, 560, quartel);
+    wallR1->color = al_map_rgba(0, 0, 0, 0);
+    wallR1->collisionEnabled = 1;
+    wallR1->collisionType = COLLISION_RECT;
+    GameObject* wallR2= createGameObject(SOLID, 2100, 1060, 260, 360, quartel);
+    wallR2->color = al_map_rgba(0, 0, 0, 0);
+    wallR2->collisionEnabled = 1;
+    wallR2->collisionType = COLLISION_RECT;
+    GameObject* wallR3 = createGameObject(SOLID, 2080, 1400, 280, 280, quartel);
+    wallR3->color = al_map_rgba(0, 0, 0, 0);
+    wallR3->collisionEnabled = 1;
+    wallR3->collisionType = COLLISION_RECT;
+
+    GameObject* wallU1 = createGameObject(SOLID, 140, 640, 2220, 10, quartel);
+    wallU1->color = al_map_rgba(0, 0, 0, 0);
+    wallU1->collisionEnabled = 1;
+    wallU1->collisionType = COLLISION_RECT;
+    GameObject* wallU2 = createGameObject(SOLID, 760, 640, 1140, 300, quartel);
+    wallU2->color = al_map_rgba(0, 0, 0, 0);
+    wallU2->collisionEnabled = 1;
+    wallU2->collisionType = COLLISION_RECT;
+
+    //FLORESTA
+    GameObject* treeL = createGameObject(SOLID, 0,0, 60, 2500, quartel);
+    treeL->color = al_map_rgba(0, 0, 0, 0);
+    treeL->collisionEnabled = 1;
+    treeL->collisionType = COLLISION_RECT;
+
+    GameObject* treeD1 = createGameObject(SOLID, 60, 2440, 1000, 60, quartel);
+    treeD1->color = al_map_rgba(0, 0, 0, 0);
+    treeD1->collisionEnabled = 1;
+    treeD1->collisionType = COLLISION_RECT;
+    GameObject* treeD2 = createGameObject(SOLID, 1280, 2440, 1220, 60, quartel);
+    treeD2->color = al_map_rgba(0, 0, 0, 0);
+    treeD2->collisionEnabled = 1;
+    treeD2->collisionType = COLLISION_RECT;
+
+    GameObject* treeR = createGameObject(SOLID, 2440, 0, 60, 2500, quartel);
+    treeR->color = al_map_rgba(0, 0, 0, 0);
+    treeR->collisionEnabled = 1;
+    treeR->collisionType = COLLISION_RECT;
+
+    GameObject* treeU = createGameObject(SOLID, 60, 0, 2380, 570, quartel);
+    treeU->color = al_map_rgba(0, 0, 0, 0);
+    treeU->collisionEnabled = 1;
+    treeU->collisionType = COLLISION_RECT;
+
+
+
 
     // - - - SINOPSE - - -
     sinopse = createScene(engine, NULL);
     sinopse->backgroundColor = al_map_rgb(20, 20, 20);
     lettersFont->size = 200;
-    sinopseTchau = createText("Final do jogo",
+    sinopseTchau = addText("Final do jogo",
         0, 0, engine->displayWidth, al_map_rgb(0, 0, 0), al_map_rgb(155, 122, 73), NULL, titleFont, 520, 550, sinopse);
 
 
